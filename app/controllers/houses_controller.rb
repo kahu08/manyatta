d4eb8590c6e75be8c07adf59e92ca248eff7378a
kashkama <owen.kamau2014@gmail.com>
2017-04-28 11:15:42 +0300

    adding new controllers and cancancan gem

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -2,13 +2,11 @@ class HousesController < ApplicationController
   before_action :set_house, only: [:show, :edit, :update, :destroy]
 
   # GET /houses
-  # GET /houses.json
   def index
     @houses = House.all
   end
 
   # GET /houses/1
-  # GET /houses/1.json
   def show
   end
 
@@ -22,43 +20,29 @@ class HousesController < ApplicationController
   end
 
   # POST /houses
-  # POST /houses.json
   def create
     @house = House.new(house_params)
 
-    respond_to do |format|
-      if @house.save
-        format.html { redirect_to @house, notice: 'House was successfully created.' }
-        format.json { render :show, status: :created, location: @house }
-      else
-        format.html { render :new }
-        format.json { render json: @house.errors, status: :unprocessable_entity }
-      end
+    if @house.save
+      redirect_to @house, notice: 'House was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /houses/1
-  # PATCH/PUT /houses/1.json
   def update
-    respond_to do |format|
-      if @house.update(house_params)
-        format.html { redirect_to @house, notice: 'House was successfully updated.' }
-        format.json { render :show, status: :ok, location: @house }
-      else
-        format.html { render :edit }
-        format.json { render json: @house.errors, status: :unprocessable_entity }
-      end
+    if @house.update(house_params)
+      redirect_to @house, notice: 'House was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /houses/1
-  # DELETE /houses/1.json
   def destroy
     @house.destroy
-    respond_to do |format|
-      format.html { redirect_to houses_url, notice: 'House was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to houses_url, notice: 'House was successfully destroyed.'
   end
 
   private
@@ -67,8 +51,8 @@ class HousesController < ApplicationController
       @house = House.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Only allow a trusted parameter "white list" through.
     def house_params
-      params.require(:house).permit(:name, :description, :telephone, :street, :rules, :ammenities, :price)
+      params.fetch(:house, {})
     end
 end
