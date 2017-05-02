f1c79622912c691ba1a05dcfaf7f2ba1fc8e1708
ndongA <boydbugle@gmai.com>
2017-05-02 17:37:39 +0300

    modify bookings controller to crud/l  via houses

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  resources :profile, :only => [:index, :edit]
+  resources :profile, only: [:index, :edit]
 
 # point controller for omniauth to devise
-  devise_for :users, controllers: { :omniauth_callbacks => "callbacks"}
+  devise_for :users, controllers: { omniauth_callbacks: "callbacks"}
 
   resources :houses
 
   resources :houses do
-    resources :reviews, :bookings
+    resources :reviews, :bookings, except: [:show]
   end
 
-  resources :landing, :only => [:index]
+  resources :landing, only: [:index]
   root to: 'landing#index'
 
 # make the index of houses into /discover
