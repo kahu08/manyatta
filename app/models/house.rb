44a593798e8106d44b91424b9aa1398f25c651fb
ndongA <boydbugle@gmai.com>
2017-05-03 14:14:01 +0300

    creates unit tests for booking model

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: houses
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  telephone   :integer
+#  location    :text
+#  street      :string
+#  bedrooms    :string
+#  minimumstay :string
+#  rules       :text
+#  ammenities  :text
+#  price       :integer
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class House < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id         :integer          not null, primary key
+#  comment    :text
+#  user_id    :integer
+#  house_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :house
