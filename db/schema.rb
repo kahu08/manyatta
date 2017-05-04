5657972bc17284f4f4cabc0b97eac1593a2f2c29
ndongA <boydbugle@gmai.com>
2017-05-04 15:09:22 +0300

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170504063111) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,17 +40,13 @@ ActiveRecord::Schema.define(version: 20170504063111) do
     t.bigint   "telephone"
     t.text     "location"
     t.string   "street"
-    t.integer  "bedrooms"
-    t.integer  "minimumstay"
+    t.string   "bedrooms"
+    t.string   "minimumstay"
     t.text     "rules"
     t.text     "ammenities"
     t.integer  "price"
     t.integer  "user_id"
-
     t.integer  "region_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.string   "images",      default: [],              array: true
